1836e9c2c4de6762f0124076c9cbff5e95c6b595
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-24 18:55:40 +0400

    Add files via upload

---
+++ releases/v1.13/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = Object.freeze({
+  SUPABASE_URL: 'https://itcbahydyqhlybofcyuh.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_qL-6DKVkAc3XWJ7JH-p2_A_-8myYRgp',
+  PUSH_PUBLIC_KEY: 'BO7UaVLOILAwgUF5mN-kle6lCGsCyxbG1GU97YM8Qs0Bl2Qv2JqDwmNgo_mLjfz7odi3YHEtlmzMdgfSyWNZc6o'
+});
